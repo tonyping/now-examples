eb5089a07a1f61805c400e1ef6e0362611c444fe
msweeneydev <mail@msweeneydev.com>
2019-08-16 11:15:36 +0100

    minor

--- build-api.js
+++ build-api.js
@@ -14,43 +14,42 @@
  */
 
 const debuglog = require('util').debuglog('API_BUILDER');
-const { lstatSync, readdirSync, createWriteStream } = require('fs');
 const { join } = require('path');
-const fse = require('fs-extra');
+const fs = require('fs-extra');
 const tar = require('tar-fs');
 
 main()
   .then(() => process.exit(0))
-  .catch((err) => {
+  .catch(err => {
     console.error(err);
     process.exit(1);
   });
 
 async function main() {
   const dist = join(__dirname, 'dist');
-  await fse.emptyDir(dist);
+  await fs.emptyDir(dist);
 
   const examples = collectExampleNames(__dirname);
   debuglog(examples);
 
   // Generate `list.json`
-  await fse.ensureDir(join(dist, '_api'));
-  await fse.writeJSON(join(dist, '_api/list.json'), examples);
+  await fs.ensureDir(join(dist, '_api'));
+  await fs.writeJSON(join(dist, '_api/list.json'), examples);
 
   // Generate 'download/*.tar.gz'
-  await fse.ensureDir(join(dist, '_api/download'));
+  await fs.ensureDir(join(dist, '_api/download'));
   await generateDownloads(__dirname, examples, join(dist, '_api/download'));
 
   // Copy `manifest.json`
   const target = join(__dirname, 'dist/_api/manifest.json');
-  await fse.copy(join(__dirname, 'manifest.json'), target);
+  await fs.copy(join(__dirname, 'manifest.json'), target);
 
   // Copy dist/_api to public for zero-config
-  await fse.copy(join(dist, '_api'), 'public');
+  await fs.copy(join(dist, '_api'), 'public');
 }
 
 async function generateDownloads(cwd, examples, downloadDir) {
-  await fse.ensureDir(downloadDir);
+  await fs.ensureDir(downloadDir);
 
   return Promise.all(
     examples.map(example => {
@@ -66,23 +65,24 @@ async function createTarball(dir, dest) {
   debuglog(`Creating ${dest}`);
 
   return new Promise((resolve, reject) => {
-    const stream = tar.pack(dir).pipe(createWriteStream(dest));
+    const stream = tar.pack(dir).pipe(fs.createWriteStream(dest));
     stream.on('close', resolve);
     stream.on('error', reject);
   });
 }
 
 function collectExampleNames(cwd) {
-  const ignores = fse.readFileSync(join(cwd, '.gitignore'), 'utf8').split('\n');
+  const ignores = fs.readFileSync(join(cwd, '.gitignore'), 'utf8').split('\n');
 
-  return readdirSync(cwd)
+  return fs
+    .readdirSync(cwd)
     .filter(name => !isDotFile(name))
     .filter(name => isDirectory(join(cwd, name)))
     .filter(name => !ignores.includes(name));
 }
 
 const isDotFile = name => name.startsWith('.');
-const isDirectory = path => lstatSync(path).isDirectory();
+const isDirectory = path => fs.lstatSync(path).isDirectory();
 
 process.on('unhandledRejection', error => {
   console.error(error);
