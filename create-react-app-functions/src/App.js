c1bf59e21d1d88726f7108669090676b0db7e337
Amandeep Singh <amandeepsingh.bajwa@gmail.com>
2019-08-30 20:14:17 +0530

    Fix 404 (#480)

--- create-react-app-functions/src/App.js
+++ create-react-app-functions/src/App.js
@@ -28,7 +28,7 @@ function App() {
       </h2>
       <p>
         <a
-          href="https://github.com/zeit/now-examples/blob/master/create-react-app-go"
+          href="https://github.com/zeit/now-examples/tree/master/create-react-app-functions"
           target="_blank"
           rel="noreferrer noopener"
         >
